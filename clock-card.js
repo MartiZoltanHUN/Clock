9f281c89775d607aee3b20a1b09bd3ba28ad7ace
MartiZoltanHUN <121397966+MartiZoltanHUN@users.noreply.github.com>
2022-12-31 15:57:48 +0100

    Update clock-card.js

--- clock-card.js
+++ clock-card.js
@@ -8,7 +8,7 @@ function leadingZero(numberString) {
 }
 
 Date.prototype.format = function (formatString) {
-    var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Szombat"];
     var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var twlv = this.getHours() == 0 ? 12 : this.getHours() > 12 ? this.getHours() - 12 : this.getHours();
     var ampm = this.getHours() >= 12 ? "PM" : "AM";
